c035bc5e1abbb17cdc0a9612c1b8708cd5317bf0
BenPark <sg2pooh@gmail.com>
2025-03-12 00:03:19 +0900

    no message

--- main_best.py
+++ main_best.py
@@ -377,7 +377,9 @@ if __name__ == "__main__":
     """
 
     SEND_EMAIL = True  # 메일 발송 여부
-    MY_COMBO_INFO = {"timeframe": "1d", "combo_params": [{"type": "RSI", "lookback": 24, "overbought": 60, "oversold": 40, "buy_time_delay": 2, "sell_time_delay": 2, "holding_period": float('inf')}]}
+    MY_COMBO_INFO = {"timeframe": "1d", "combo_params": [{"type": "RSI", "lookback": 2, "overbought": 60, "oversold": 40, "buy_time_delay": 2, "sell_time_delay": 2, "holding_period": 12}]}
+
+
 
 
 
